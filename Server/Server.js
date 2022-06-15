f0802e1e3509f8aeb77da4d8bee656300232850a
Tuba Asif <68794712+tuba-theProgrammer@users.noreply.github.com>
2022-06-15 14:01:45 +0500

    done-commit

--- Server/Server.js
+++ Server/Server.js
@@ -1,6 +1,8 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 app.set("view engine","ejs")
+
+
 const io = require('socket.io')(server, {
   cors: {
     origin: '*',
@@ -11,18 +13,37 @@ var cors = require('cors')
 app.use(cors());
 
 
+io.use((socket, next) => {
+    const username = socket.handshake.auth.username;
+    if (!username) {
+      return next(new Error("invalid username"));
+    }
+    socket.username = username;
+    console.log(socket.username)
+    next();
+  });
+
+
+
+
 var clients = [];
 
-app.get('/', (req, res)=>{
+app.get('/',(req, res)=>{
   clients = []
-  io.sockets.sockets.forEach((socket) => {
-    clients.push(socket); 
-});
 
+for (let [id, socket] of io.of("/").sockets) {
+  clients.push({
+    userID: id,
+    username: socket.username,
+  });
+}
   res.render("serverSide",{
      clients
    })
 });
+
+
+
 
 app.get('/RemoveClient', (req,res)=>{
   const {id}= req.query
@@ -37,22 +58,52 @@ app.get('/RemoveClient', (req,res)=>{
  
 })
 
-app.get('/SendBroadCastMsg',(req,res)=>{
-  io.emit("msg", "world");
-   console.log(" i am here in broadcast route")
+app.post('/SendBroadCastMsg', async (req,res)=>{
+  console.log( await req.body)
   res.redirect('/')
+ 
 })
 
 
-io.on('connection',function(socket){
+app.get('/NotifyClient',(req,res)=>{
+  const {id}= req.query
+  console.log(id)
+  const content= "notificaton from Server"
+  io.sockets.sockets.forEach((socket) => {
+    if(socket.id === id)
+      {  
+      
+          socket.emit("msg", {
+            content,
+            to: socket.id
+          });  
+         res.redirect('/')
+      }
+     
+});
+ 
+})
+
+app.get('/stopConnection',async (req,res)=>{
+  io.sockets.sockets.forEach((socket) => {
+        socket.disconnect(true);
+});
+   res.redirect('/')
+}
+)
+
+
+app.get('/startConnection',async (req,res)=>{
+  
+  io.on('connection',function(socket){
     console.log('a new user is connected')
     socket.emit('connection', null);
-   
+})
+res.redirect('/')
 })
 
 
 
- 
 server.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,51 +1,107 @@
 import './App.css';
 import React, { useState, useEffect } from "react";
-import socketClient  from "socket.io-client";
+import socketClient from "socket.io-client";
 import { Button } from 'react-bootstrap';
 const SERVER = "http://127.0.0.1:3003";
 
 function App() {
-  const [ClientName,setClientName]= useState("ClientName")
-  const [ClientID,setClientID]= useState("ClientID")
-  const [buttonState,setbuttonState]= useState(false)
-  const [broadcastMsg,setBroadCastMsg] = useState('hello there')
 
-  var socket = ''
+var socket = '';
 
-  function ConnectToServer(){
-    socket = socketClient (SERVER);
-    socket.on('connection', () => { 
-      console.log(`I'm connected with the back-end`);
-      setClientID(socket.id)
-      socket.on("msg", (arg) => {
-        setBroadCastMsg(arg)
-      });
-     
-    });
-    setbuttonState(true)
+const [ClientID,setClientID]= useState("ClientID")
+const [buttonState,setbuttonState]= useState(false)
+const [ClientUsername,setClientUsername] = React.useState("");
+
+
+
+function ConnectToServer(){
+socket.on('connection', () => { 
+console.log(`I'm connected with the back-end`);
+setClientID(socket.id)
+socket.on("msg", (arg) => {
+  const {content} = arg
+  if(!content){
+    console.log("Msg Not Found")
+  }else{
+    console.log(content)
+  
+  } 
+  });
+});
+setbuttonState(true)
+}
+
+
+
+const handleChange = (e) => {
+setClientUsername(e.target.value);
+};
+
+
+
+const handleSubmit = (e) => {
+e.preventDefault()
+//usernameAlreadySelected = true;
+socket = socketClient.connect(SERVER, {
+  auth:{
+    username: ClientUsername
   }
-
-  return (
-   <>
-       <div style={{marginTop:"200px",marginLeft:"100px"}}> 
-       <h4>{broadcastMsg}</h4>
-       <h3> Name: {ClientName}</h3>
-       <h3>ID: {ClientID}</h3>
-       <Button style={{ 
-      width: "200px", 
-      height: "50px",  
-      border: "5px", 
-      backgroundColor: "darkslategray",
-      borderRadius: "17px" }}    disabled={buttonState} onClick={ConnectToServer}> 
-      Connect to Server
-      </Button>
-      <div> 
-        
-      </div>
-       </div>
-   
-   </>
+}
   );
+ConnectToServer()
+
+};
+
+
+
+
+
+return (
+<>
+<div style={{marginTop:"200px",marginLeft:"100px"}}>
+<form  >
+<input style={{border: "5px solid black", width:"200px", height:'50px' }} type="text" onChange={handleChange}/>
+<input type="submit" style={{ 
+width: "200px", 
+height: "50px", 
+border: "5px", 
+color:"white",
+backgroundColor: "darkslategray",
+borderRadius: "17px" }} value="Connect to Server"
+onClick={handleSubmit} disabled={buttonState}/>
+</form>
+<div>
+<h3>ID: {ClientID}</h3>
+</div>
+
+
+<div> 
+<h4>Notifications</h4>
+<div>
+
+
+</div>
+</div>
+
+
+<div>
+<form  >
+<input style={{border: "5px solid black", width:"200px", height:'50px' }} type="text" />
+<input type="submit" style={{ 
+width: "200px", 
+height: "50px", 
+border: "5px", 
+color:"white",
+backgroundColor: "darkslategray",
+borderRadius: "17px" }} value="Send Message"
+ disabled={buttonState}/>
+</form>
+</div>
+
+</div>
+</>
+);
 }
 
 export default App;
+
